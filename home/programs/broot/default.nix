ed5adf82cd972f0ea6312ea2f389e1f7e7de440c
Rafael Alvarez <ralvarez@fluidattacks.com>
2020-12-07 01:19:18 -0500

    feat(front): broot config and install

---
+++ home/programs/broot/default.nix
@@ -0,0 +1,22 @@
+{config, lib, pkgs, ... }:
+
+{
+
+  programs.broot = {
+    enable = true;
+    enableZshIntegration = true;
+    verbs = [
+      { invocation = "edit";
+        key = "enter";
+        shortcut = "e";
+        execution = "$EDITOR {file}";
+        leave_broot = false; }
+      { invocation = "preview_text";
+        key = "enter";
+        shortcut = "bat";
+        execution = "bat {file}";
+        leave_broot = false; }
+    ];
+  };
+
+}
