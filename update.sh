abc254645d8361147da8bf3c538752327e049ebc
Rafael Alvarez <ralvarez@fluidattacks.com>
2020-12-20 14:39:52 -0500

    fix(build): shellcheck varnames

--- update.sh
+++ update.sh
@@ -9,5 +9,5 @@ sudo nixos-rebuild --flake .#auto switch
 
 # clean
 MAX_AGE="30d"
-nix-collect-garbage --delete-older-than "$MAX_AGE"
-sudo nix-collect-garbage --delete-older-than "$MAX_AGE"
+nix-collect-garbage --delete-older-than "${MAX_AGE}"
+sudo nix-collect-garbage --delete-older-than "${MAX_AGE}"
