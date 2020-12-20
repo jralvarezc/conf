4426c668d7546c9c7fbf8afe8993aef900ba22a4
Rafael Alvarez <ralvarez@fluidattacks.com>
2020-12-20 14:36:47 -0500

    feat(build): safer scripts

--- format.sh
+++ format.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
 set -x
 
 find -iname '*nix' -exec \
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
 set -x
-set -e
 
 # update
 nix flake update --recreate-lock-file
