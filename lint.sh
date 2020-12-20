87bea12da1abb0ad44477b7291148d4aa8cdf8f3
Rafael Alvarez <ralvarez@fluidattacks.com>
2020-12-20 14:22:01 -0500

    feat(build): linter for nix files

---
+++ lint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -x
+
+nix-linter \
+    -W DIYInherit \
+    -W EmptyInherit \
+    -W EmptyLet \
+    -W EtaReduce \
+    -W FreeLetInFunc \
+    -W LetInInheritRecset \
+    -W ListLiteralConcat \
+    -W NegateAtom \
+    -W SequentialLet \
+    -W SetLiteralUpdate \
+    -W UnfortunateArgName \
+    -W UnneededRec \
+    -W UnusedArg \
+    -W UnusedLetBind \
+    -W UpdateEmptySet \
+    -W AlphabeticalArgs \
+    -W AlphabeticalBindings \
+    -W BetaReduction \
+    -W EmptyVariadicParamSet \
+    -W UnneededAntiquote \
+    -r .
