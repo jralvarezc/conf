8ff88d58e24c03f0e264a584aca06ff0eb3c8a39
Rafael Alvarez <i@jralvarezc.com>
2021-01-09 14:51:33 -0500

    feat(back): add nixos-hardware support fo x1c6

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages."${system}";
@@ -26,6 +27,7 @@
           ./hardware/x1c6
           # external
           home-manager.nixosModules.home-manager
+          nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
         ];
       };
 
