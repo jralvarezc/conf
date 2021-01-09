979f3b35894682e94de600ba806722a484a2d80b
Rafael Alvarez <i@jralvarezc.com>
2021-01-09 15:28:28 -0500

    refac(back): hw specific cfg in his own file

--- hosts/auto/default.nix
+++ hosts/auto/default.nix
@@ -1,8 +1,23 @@
 { config, pkgs, ... }:
 
 {
+
+  system = {
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "19.03";
+    autoUpgrade.enable = false;
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/bd7087d7-6041-4b65-81a8-f88364efe7ab";
+    fsType = "ext4";
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/2570bcfb-f35f-4ff9-b6ab-e93d8562b6a5"; }
+  ];
+
   imports = [
-    ../../profiles/storage.nix
     ../../profiles/boot.nix
     ../../profiles/nix.nix
     ../../profiles/nixpkgs.nix
@@ -19,11 +34,5 @@
     ../../profiles/sound.nix
     ../../modules/vpn.nix
   ];
-
-  system = {
-    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-    stateVersion = "19.03";
-    autoUpgrade.enable = false;
-  };
 
 }
