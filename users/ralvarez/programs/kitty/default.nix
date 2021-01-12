3cc10d7132c586412be5836f331564d979b9e78b
Rafael Alvarez <i@jralvarezc.com>
2021-01-11 23:46:07 -0500

    feat(font): include unicode test doc

--- users/ralvarez/programs/kitty/default.nix
+++ users/ralvarez/programs/kitty/default.nix
@@ -5,4 +5,8 @@
   xdg.configFile."kitty/kitty.conf".source = ./kitty.conf;
   xdg.configFile."kitty/theme.conf".source = ./themes/tango-dark.conf;
 
+  # test fonts and terminal encoding
+  # $ http https://antofthy.gitlab.io/info/data/unicode_examples/UTF-8-demo.txt | less
+  # $ http https://unicode.org/Public/emoji/5.0/emoji-test.txt | less
+
 }
